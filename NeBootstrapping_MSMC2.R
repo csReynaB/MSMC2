6d63a6b35c98f504b53be82f631e7d0c7b0d1bb1
csReynaB <creyna604@gmail.com>
2021-09-09 11:33:31 +0200

    adapt rCRR bootstraping

--- NeBootstrapping_MSMC2.R
+++ NeBootstrapping_MSMC2.R
@@ -117,11 +117,34 @@ createTable1 <- function(suffix){
 }
 
 setwd("C://Users/creyn/Dropbox/Backup/MSMC2/withKK1/multi/rCCR/bootstrapping") 
+setwd("/home/reynac/Dropbox/Backup/MSMC2/withKK1/multi/rCCR/bootstrapping/")
 
-df.msmc <- createTable(suffix="final.txt")  
+
+df.msmc <- createTable1(suffix="final.txt")  
 info <- read.table("samplelist", header = T)
 
-pdf("supFig_bootstrappingcRR_MSMC2.pdf", width=183*0.039370 , height=183*0.039370)
+l.clw <- list(
+  "ZagrosRegion" = list("brown",c(1,1.6), 'A'),
+  #"NWAnatolia" = list(	"#CCCC00",c(1,1.6),'B'),
+  "NorthernGreece" = list("dodgerblue",c(1,1.6),'B'),
+  "CentralSerbia" = list(brewer.pal(n=9, name = "Greens")[c(7)], c(1,1.6), 'C') , 
+  
+  "Hungary-Neo" = list("blue",c(1,1.6),'D'), 
+  
+  "LowerAustria" = list( "green",c(1,1.6),'E'),
+  "SouthernGermany1" = list(brewer.pal(n=9, name = "Purples")[c(5)],c(1,1.6), 'F'), 
+  "SouthernGermany2" =  list(brewer.pal(n=9, name = "Purples")[c(7)],c(1,1.6), 'G'),
+  
+  "Lepenski-Vir" = list("black",c(3,4), 'H'),
+  
+  "NorthernEurope-Meso" = list("orange3",c(2,2.4), 'I'),
+  "Caucasus" = list("#F0E442", c(2,2.4), 'J'),
+  "WesternEurope-Meso" = list("#FF69B4",c(2,2.4), 'K'),
+  "DanubeGorges-Meso" =  list(brewer.pal(n=9, name = "Reds")[c(5)],c(2,2.4), 'L')
+)
+
+
+pdf("supFig_bootstrapping_rCRR_MSMC2.pdf", width=183*0.039370 , height=183*0.039370)
 #png("supFig_bootstrappingcRR_MSMC2.png", width=1100, height=1100, res=82)
 par(oma = c(3, 1, 1, 1), mai=c(.6,.6,.2,.2) )
 layout.matrix <- matrix(c(1:12), nrow = 4, ncol = 3)
@@ -135,19 +158,19 @@ layout(mat = layout.matrix,
     samples <- info[info$Region %in% p,]$Sample
     tmp <- df.msmc[df.msmc$Pop1 %in% samples,]
 #unique(tmp$Pop2)[c(9,6,1,7,3,4,11,10,8,2,5)] no bon002
-    for (i in unique(tmp$Pop2)[c(10,7,1,8,4,5,12,11,9,2,6,3)]){
-      #i <- "Asp6-Klein7"
+    for (i in unique(tmp$Pop2)[c(12,8,10,7,1,3,4,6,9,5,2,11)]){
+      #i <- "NE1"
       pop <- as.character(info[info$Sample %in% i,]$Region)
       tmp2 <- tmp[tmp$Pop2 %in% i, ]
       #splitTime <- getCCRintersect(tmp2, 0.5)
       #df.ccR <- rbind(df.ccR, data.frame(Pop1=p, Pop2=pop, Ind1=samples, Ind2=s, DivergenceTime=splitTime))
       
       plot(tmp2$left_time_boundary/mu*gen, 2 * tmp2$lambda01 / (tmp2$lambda00 + tmp2$lambda11), 
-           xlim=c(1000,0.5e+05),ylim=c(0,1), log="x", type="n", xlab="", ylab="", cex.axis=1.4)
+           xlim=c(1000,0.5e+05),ylim=c(0,1), log="x", type="n", xlab="", ylab="", cex.axis=0.8)
     
-      #mtext(l.clw[[p]][[3]], side=3, line=1, adj=0, cex=1, col="black", outer=F, font = 2 )
-      mtext(side=1, line=3, "Years ago", col="black", font=1,cex=1.3)
-      mtext(side=2, line=3, paste("CCR from",p,sep = " "), col="black", font=1, cex=1.3)
+      mtext(l.clw[[pop]][[3]], side=3, line=1, adj=0, cex=0.6, col="black", outer=F, font = 2 )
+      mtext(side=1, line=3, "Years ago", col="black", font=1,cex=0.6)
+      mtext(side=2, line=3, "CCR from NW Anatolia", col="black", font=1, cex=0.6)
       abline(v=20000, lty=2,col="black")
       abline(h=0.5,lty=2,col="black")
       
@@ -161,7 +184,7 @@ layout(mat = layout.matrix,
       tmp2 <- tmp2[tmp2$Rep %in% "final", ]
       lines(tmp2$left_time_boundary/mu*gen, 2 * tmp2$lambda01 / (tmp2$lambda00 + tmp2$lambda11),  
             lty=l.clw[[pop]][[c(2,1)]], lwd=2, type="s", col=l.clw[[pop]][[1]])
-      text(1e+03, 0.8, i, cex=1.4,adj=0)
+      text(1e+03, 0.8, i, cex=0.8,adj=0)
     }
     
   #}
